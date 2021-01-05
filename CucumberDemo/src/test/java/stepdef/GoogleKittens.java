c55db51437d82e6cbf56696b0f570879526e2709
benjamin.oluyomi <boluyomi@academytrainee.com>
2021-01-05 17:00:00 +0000

    cucumber demo

---
+++ CucumberDemo/src/test/java/stepdef/GoogleKittens.java
@@ -0,0 +1,27 @@
+package stepdef;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class GoogleKittens {
+	
+	@Given("^I can open Google$")
+	public void i_can_open_Google() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@When("^I search for kittens$")
+	public void i_search_for_kittens() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("^Google will give me Kittens$")
+	public void google_will_give_me_Kittens() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+}
