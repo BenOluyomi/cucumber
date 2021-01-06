a0ef817f5cb2dc9b1b1d92424f4433ae882734eb
benjamin.oluyomi <boluyomi@academytrainee.com>
2021-01-06 10:45:02 +0000

    starting tea proj

--- CucumberDemo/src/test/java/runner/TestRunner.java
+++ CucumberDemo/src/test/java/runner/TestRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/cucumber/kittens.feature", glue = {"stepdef"})
+@CucumberOptions(features = "src/test/resources/cucumber/kittens.feature", monochrome = true, glue = {"stepdef"})
 public class TestRunner {
 
 }
--- CucumberDemo/src/test/java/stepdef/GoogleKittens.java
+++ CucumberDemo/src/test/java/stepdef/GoogleKittens.java
@@ -1,26 +1,57 @@
 package stepdef;
 
+import static org.junit.Assert.assertEquals;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class GoogleKittens {
 	
+	private static WebDriver driver;
+	
+	private static String URL = "https://www.google.com/";
+	
+	@Before
+	public static void init() {
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe"); // set driver path
+	    driver =new ChromeDriver(); 
+	}
+	
+	@After
+	public void cleanUp() {
+		driver.quit();
+	}
+	
 	@Given("^I can open Google$")
 	public void i_can_open_Google() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
+	   driver.get(URL);
+	   assertEquals("Google", driver.getTitle());
+	   System.out.println("Google is open");
 	    
 	}
 
 	@When("^I search for kittens$")
 	public void i_search_for_kittens() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
+	    WebElement input = driver.findElement(By.name("q"));
+	    input.sendKeys("kittens");
+	    input.submit();
+	    System.out.println("Searched for kittens");
 	    
 	}
 
 	@Then("^Google will give me Kittens$")
 	public void google_will_give_me_Kittens() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
+	   driver.findElement(By.name("q")).clear();
+	   driver.getPageSource().contains("kittens");
+	   System.out.println("Found Kittens");
 	    
 	}
 
