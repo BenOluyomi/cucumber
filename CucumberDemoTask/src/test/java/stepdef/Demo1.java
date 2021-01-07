5ba645d3d631bcbc184467849c38e9d9a9c48315
benjamin.oluyomi <boluyomi@academytrainee.com>
2021-01-07 13:06:38 +0000

    cucumber task1 complete

--- CucumberDemoTask/src/test/java/stepdef/Demo1.java
+++ CucumberDemoTask/src/test/java/stepdef/Demo1.java
@@ -1,11 +1,13 @@
 package stepdef;
-
+import java.util.HashMap;
+import java.util.Map;
 import static org.junit.Assert.assertEquals;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
@@ -22,13 +24,15 @@ public class Demo1 {
 	@Before
 	public static void init() {
 		System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe"); // set driver path
-	    driver = new ChromeDriver(); 
+		ChromeOptions options = new ChromeOptions();
+
+		driver = new ChromeDriver(chromeCfg());
 	}
 	
 	@After
 	public void cleanUp() {
-		//driver.quit();
-	}
+		driver.quit();
+	} 
 	
 	@Given("^Open the create page$")
 	public void open_the_create_page() throws Throwable {
@@ -38,6 +42,7 @@ public class Demo1 {
 
 	@When("^Input information$")
 	public void input_information() throws Throwable {
+		
 		WebElement username = driver.findElement(By.name("username"));
 		WebElement password = driver.findElement(By.name("password"));
 		
@@ -55,22 +60,40 @@ public class Demo1 {
 
 	@Then("^A user will be created$")
 	public void a_user_will_be_created() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
+		 driver.getPageSource().contains("david1010");
 	    
 	}
 
 	@Given("^The login page opens$")
 	public void the_login_page_opens() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    
+		driver.get(URL+"/login.php");
+		   //WebElement login = driver.findElement(By.xpath("/html/body/div/center/table/tbody/tr[2]/td/div/center/table/tbody/tr/td[2]/p/small/a[4]"));
+	       // login.click();
 	}
 
 	@Then("^A user will be logged in$")
 	public void a_user_will_be_logged_in() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    
+		WebElement log = driver.findElement(By.name("FormsButton2"));					
+        driver.findElement(By.name("username")).sendKeys("david1010");							
+        driver.findElement(By.name("password")).sendKeys("test");							
+        log.click();					
+        System.out.println("Login Done with Submit");					
+        driver.getPageSource().contains("**Successful Login**");
 	}
+	public static ChromeOptions chromeCfg() {
+		Map<String, Object> prefs = new HashMap<String, Object>();
+		ChromeOptions cOptions = new ChromeOptions();
 
+		// Settings
+		prefs.put("profile.default_content_setting_values.cookies", 2);
+		prefs.put("network.cookie.cookieBehavior", 2);
+		prefs.put("profile.block_third_party_cookies", true);
+
+		// Create ChromeOptions to disable Cookies pop-up
+		cOptions.setExperimentalOption("prefs", prefs);
+
+		return cOptions;
+	}
 
 
 }
